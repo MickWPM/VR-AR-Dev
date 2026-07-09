659f14f9d713b93116cf344d4df7fa48b305a9dc
Mick M <mickwpmcdonnell@gmail.com>
2026-07-09 11:51:36 +0930

    Simple bin implemented

--- Task-1/Assets/Scripts/ConveyerForce.cs
+++ Task-1/Assets/Scripts/ConveyerForce.cs
@@ -11,32 +11,9 @@ public class ConveyerForce : MonoBehaviour
             return;
 
         //rb.AddForce(transform.forward * forcePower, ForceMode.VelocityChange);
+        
         rb.linearVelocity = transform.forward * forcePower;
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        var colliderGO = collision.gameObject;
-        Rigidbody rb = colliderGO.GetComponent<Rigidbody>();
-        if (rb == null)
-            return;
 
-        SetRotationConstraints(rb, true);
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        var colliderGO = collision.gameObject;
-        Rigidbody rb = colliderGO.GetComponent<Rigidbody>();
-        if (rb == null)
-            return;
-
-        SetRotationConstraints(rb, false);
-    }
-
-    private void SetRotationConstraints(Rigidbody rb, bool constraintEnabled)
-    {
-        return;
-        rb.constraints = constraintEnabled ? RigidbodyConstraints.FreezeRotation : RigidbodyConstraints.None;
-    }
 }
