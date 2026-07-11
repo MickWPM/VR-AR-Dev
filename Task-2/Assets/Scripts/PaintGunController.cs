457b127901f0281320ad72c620e273578228b3d3
Mick M <mickwpmcdonnell@gmail.com>
2026-07-11 19:12:39 +0930

    Colour sampling functionality implemented

--- Task-2/Assets/Scripts/PaintGunController.cs
+++ Task-2/Assets/Scripts/PaintGunController.cs
@@ -6,15 +6,79 @@ public class PaintGunController : MonoBehaviour
     [SerializeField] private Painter projectilePrefab;
 
     public Color paintingColour = Color.white;
+    [SerializeField] private bool currentlySamplingColour = false;
+    [SerializeField] private Color currentSampleColour = Color.white;
+    [SerializeField] private Renderer colourPreviewRenderer;
+
+    private void Awake()
+    {
+        colourPreviewRenderer.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        bool samplingColourThisFrame = CheckSampleColour();
+        SetColourSampleState(samplingColourThisFrame);
+    }
+
+    public void SetColourSampleState(bool sampleColours)
+    {
+        if (sampleColours == currentlySamplingColour) return;
+
+        currentlySamplingColour = sampleColours;
+        currentSampleColour = paintingColour;
+        colourPreviewRenderer.gameObject.SetActive(currentlySamplingColour);
+    }
+
+    private bool CheckSampleColour()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(muzzleExit.position, muzzleExit.forward, out hit, Mathf.Infinity))
+        {
+            var sampleSurface = hit.collider.gameObject.GetComponent<ColourSamplingSurface>();
+            if (sampleSurface == null) return false;
+
+            DrawLaser(hit);
+
+            var texCoord = hit.textureCoord;
+
+            Renderer renderer = hit.collider.GetComponent<Renderer>();
+            Texture2D tex = renderer.sharedMaterial.mainTexture as Texture2D;
+            currentSampleColour = tex.GetPixelBilinear(texCoord.x, texCoord.y);
+            colourPreviewRenderer.material.SetColor("_BaseColor", currentSampleColour);
+            return true;
+        }
+        return false;
+    }
+
+    public void DrawLaser(RaycastHit hit)
+    {
+        Vector3 dir = hit.point - transform.position;
+        Debug.DrawRay(muzzleExit.position, dir, Color.red);
+    }
+
 
     public void Fire()
     {
-        Fire(projectilePrefab);
+        if (currentlySamplingColour)
+        {
+            SetPaintingColour(currentSampleColour);
+        }
+        else
+        {
+            Fire(projectilePrefab);
+        }
     }
 
     public void Fire(Painter prefab)
     {
         var painter = Instantiate(prefab, muzzleExit.transform.position, muzzleExit.transform.rotation) as Painter;
         painter.SetColour(paintingColour);
+    }
+
+    public void SetPaintingColour(Color colour)
+    {
+        paintingColour = colour;
     }
 }
