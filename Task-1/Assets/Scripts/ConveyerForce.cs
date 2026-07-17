37504e23bd5aad5234c0f0e8690a55d03b8c20c6
Mick M <mickwpmcdonnell@gmail.com>
2026-07-17 19:29:54 +0930

    Add comments to conveyer force script

--- Task-1/Assets/Scripts/ConveyerForce.cs
+++ Task-1/Assets/Scripts/ConveyerForce.cs
@@ -10,6 +10,8 @@ public class ConveyerForce : MonoBehaviour
         if (rb == null)
             return;
 
+        //This approach works fine too however the direct velocity control has a 
+        //better 'feel' in testing. Left in here to highlight an alternative approach
         //rb.AddForce(transform.forward * forcePower, ForceMode.VelocityChange);
         
         rb.linearVelocity = transform.forward * forcePower;
