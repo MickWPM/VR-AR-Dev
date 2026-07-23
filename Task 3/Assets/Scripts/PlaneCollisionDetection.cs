989bec11ec42ab097405a3d66b4a62aa06b51211
Mick M <mickwpmcdonnell@gmail.com>
2026-07-23 13:27:16 +0930

    Initial prototype complete

--- Task 3/Assets/Scripts/PlaneCollisionDetection.cs
+++ Task 3/Assets/Scripts/PlaneCollisionDetection.cs
@@ -5,9 +5,28 @@ public class PlaneCollisionDetection : MonoBehaviour
     public GameObject onImpactSpawn;
     private void OnTriggerEnter(Collider other)
     {
+        var hanger = other.gameObject.GetComponent<RunwaySafeHanger>();
+        Debug.Log("Hit : ", other.gameObject);
+        if (hanger != null)
+        {
+            SuccessfulLanding(hanger);
+        } else
+        {
+            Collision();
+        }
+    }
 
+
+    private void Collision()
+    {
         Debug.Log("Collision!", gameObject);
         Instantiate(onImpactSpawn, transform.position, Quaternion.identity);
+        Destroy(gameObject.transform.root.gameObject);
+    }
+
+    private void SuccessfulLanding(RunwaySafeHanger hanger)
+    {
+        Debug.Log("Safe landing at hanger");
         Destroy(gameObject.transform.root.gameObject);
     }
 }
