759fa90b1887993e62e655f348ba519e2d4ca089
Mick M <mickwpmcdonnell@gmail.com>
2026-08-06 11:23:27 +0930

    Initial implementation

---
+++ Task 5/Assets/Scripts/Geometry.cs
@@ -0,0 +1,112 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Geometry : MonoBehaviour
+{
+    public GameObject rayObject;
+    public GameObject markerPrefab;
+    private GameObject marker;
+
+    public GameObject meshGeometry;
+    public GameObject planeGeometry;
+
+    private int[] originalFaces;
+
+    private void Start()
+    {
+        Mesh mesh = meshGeometry.GetComponent<MeshFilter>().mesh;
+        originalFaces = mesh.triangles;
+
+        marker = Instantiate(markerPrefab);
+    }
+
+
+    void Update()
+    {
+        //PlaneCutGeometry();
+        RayCutGeometry();
+    }
+
+    void RayCutGeometry()
+    {
+        Ray ray = new Ray(rayObject.transform.position, rayObject.transform.forward);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit)) 
+        {
+            marker.transform.position = hit.point;  
+        }
+
+        Mesh mesh = meshGeometry.GetComponent<MeshFilter>().mesh;
+        Vector3[] verts = mesh.vertices;
+        int[] faces = mesh.triangles;
+        int[] newFaces = new int[faces.Length];
+        int newFaceIndex = 0;
+
+        for (int i = 0; i < faces.Length; i+=3)
+        {
+            bool hasHit = false;
+            for (int j = 0; j < 3; j++)
+            {
+                int vert = faces[i+j];
+                Vector3 V = meshGeometry.transform.TransformPoint(verts[vert]);
+                Vector3 P = ray.origin;
+                Vector3 v1 = V - P;
+                Vector3 dhat = ray.direction.normalized;
+                Vector3 v2 = Vector3.Dot(v1, dhat) * dhat;
+                float distance = (v1 - v2).magnitude;
+
+                if (distance < 0.02f)
+                {
+                    hasHit = true;  
+                }
+            }
+
+            if (!hasHit)
+            {
+                newFaces[newFaceIndex++] = faces[i];
+                newFaces[newFaceIndex++] = faces[i + 1];
+                newFaces[newFaceIndex++] = faces[i + 2];
+            }
+        }
+        mesh.triangles = newFaces;
+    }
+
+    void PlaneCutGeometry()
+    {
+        Plane plane = new Plane(planeGeometry.transform.up, planeGeometry.transform.position);
+        Mesh mesh = meshGeometry.GetComponent<MeshFilter>().mesh;
+
+        
+
+        Vector3[] verts = mesh.vertices;
+        Vector2[] uvs = mesh.uv;
+        int[] faces = originalFaces;
+        int[] newFaces = new int[faces.Length];
+        int newFaceIndex = 0;
+
+        for (int i = 0; i < faces.Length; i+=3)
+        {
+            int v1=faces[i];
+            int v2=faces[i+1];
+            int v3=faces[i+2];
+            if ((plane.GetSide(meshGeometry.transform.TransformPoint(verts[v1]))) &&
+                (plane.GetSide(meshGeometry.transform.TransformPoint(verts[v2]))) &&
+                (plane.GetSide(meshGeometry.transform.TransformPoint(verts[v3]))))
+            {
+                newFaces[newFaceIndex++] = v1;
+                newFaces[newFaceIndex++] = v2;
+                newFaces[newFaceIndex++] = v3;
+                //uvs[i] = new Vector2(0.1f, 0.5f);
+            } else
+            {
+                //uvs[i] = new Vector2(0.9f, 0.5f);
+            }
+        }
+
+        //mesh.vertices = verts;
+        //mesh.uv = uvs;  
+        mesh.triangles = newFaces;
+    }
+}
